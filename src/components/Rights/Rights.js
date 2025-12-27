0cff745f82d4dda1ebac5d8e2184dbe62c4e01b1
Dvir Magen <Dvirma@mta.ac.il>
2025-12-27 13:30:29 +0200

    Fix Rights Component Design

--- src/components/Rights/Rights.js
+++ src/components/Rights/Rights.js
@@ -1,11 +1,19 @@
+import './Rights.css';
+
 export default function Rights(){
     return(
         <section id="rights-reserved">
-        <p>
-        All content and design elements on this website<br />
-        were meticulously crafted by Dvir Magen.
-        </p>
-        <p>©2024 Dvir Magen. All Rights Reserved.</p>
-    </section>
+            <div className="rights-container">
+                <div className="rights-content">
+                    <p className="rights-description">
+                        All content and design elements on this website were meticulously crafted by
+                    <span className="rights-accent"> Dvir Magen</span>.
+                    </p>
+                    <p className="rights-copyright">
+                        ©2024 Dvir Magen. All Rights Reserved.
+                    </p>
+                </div>
+            </div>
+        </section>
     )
 }
